a5d5cc742eb07dd79efdb606babd943246ef2b29
Gabriel Antunes <gbr.antunes@gmail.com>
2019-11-29 16:51:39 -0400

    getUrl() adicionado

--- App/init.php
+++ App/init.php
@@ -4,8 +4,24 @@ namespace App;
 
 class Init{
 
+    private $routes;
+
     public function __construct(){
-        echo 'Carregou.';
+        $this->initRoutes();
+    }
+
+    public function initRoutes(){
+        $arr['home'] = ['route' => '/', 'controller' => 'index', 'action' => 'index'];
+        $arr['empresa'] = ['route' => '/empresa', 'controller' => 'index', 'action' => 'empresa'];
+        $this->setRoutes($arr);
+    }
+
+    public function setRoutes(array $routes){
+        $this->routes = $routes;
+    }
+
+    public function getUrl(){
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
 }
